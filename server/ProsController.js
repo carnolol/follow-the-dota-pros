37324b6799a86b015f9dc52b19d1263a6e96c2fc
Mike Chadwick <Harakili@gmail.com>
2020-04-30 12:43:55 -0600

    sdfks

--- server/ProsController.js
+++ server/ProsController.js
@@ -2,6 +2,7 @@ module.exports = {
     getProPlayers: async (req, res) => {
         const db = req.app.get('db')
         const pros = await db.get_pro_players()
+        console.log(pros)
         res.status(200).send(pros)
     }
 }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,17 +1,32 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import './Landing.css'
 
 function Landing(props) {
-    const[]
+    const [pros, setPros] = useState([])
 
-    const getProPlayers = () => {
-        axios.get('/dota-pros/pros')
-    }
+    useEffect(() => {
+        axios.get('/dota-pros/pros').then(res => {
+            console.log('I AM THE DATA:', res.data)
+            setPros(res.data)
+        }).catch(err => console.log(err))
+    }, [])
+    const proPlayers = pros.map(pro => {
+        return <div className='pro-container'>
+            <img className='pro-picture'
+                alt='pro'
+                src={pro.picture} />
+            <div>
+                <h3>{pro.name}</h3>
+                <br/>
+                <p>Has won ${pro.winnings} in his career!</p>
+            </div>
+        </div>
+    })
     return (
         <div className='main-landing-div'>
-                <h1>landing JS</h1>
+            {proPlayers}
         </div>
     )
 }
