4ec89674ee67af6e81f45817be15f9c7743467fd
Mike Chadwick <Harakili@gmail.com>
2020-05-21 22:39:50 -0600

    trying to update icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,12 @@ function Footer() {
                 <a href='https://www.linkedin.com/in/michael-chadwick91/'>
                     <img className='linkin-logo'
                         alt='linkedin logo'
-                        src='https://golflifenavigators.com/wp-content/uploads/2018/09/linkedin-logo.png'    />
+                        src='https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg' />
+                </a>
+                <a href='https://github.com/carnolol'>
+                    <img className='github-logo'
+                        alt='github logo'
+                        src='https://cdn.iconscout.com/icon/free/png-512/github-153-675523.png'/>
                 </a>
             </div>
         </div>
